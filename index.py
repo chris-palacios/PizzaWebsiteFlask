8eb85df60bec26c3ff8d1d11b6f19503c0f72df6
Christian <christian.a.palacios98@gmail.com>
2023-07-28 11:07:52 -0700

    new

--- index.py
+++ index.py
@@ -1,9 +1,25 @@
+#!/bin/python
+
 from flask import Flask, request, render_template
 
-app = (__name__)
+app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return "Hello"
+    return render_template('home.html')
+@app.route('/home.html')
+def home():
+    return render_template('home.html')
+@app.route('/menu.html')
+def menu():    
+    return render_template('menu.html')
 
-@app.route("/")
+@app.route('/aboutUs.html')
+def aboutUs():
+    return render_template('aboutUs.html')
+
+@app.route('/contactUs.html')
+def contactUs():
+    return render_template('contactUs.html')
+
+app.run(host="0.0.0.0", port=5008)
